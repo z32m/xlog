f590a5fed5aff0c571973125903c87e8f54a0d93
Serhii Kuzmychov <ku3cmich@gmail.com>
2024-01-13 22:43:16 +0200

    minor

--- xlog.h
+++ xlog.h
@@ -2,6 +2,7 @@
 #define __xlog_h__
 
 #define LOG_RESULT(action, ...) LOG_DBG("%s = %d", #action, action(__VA_ARGS__))
+
 #define ASSERT_RESULT(action, ...)                      \
     {                                                   \
         LOG_DBG("performing: %s", #action); \
