e8f22dee58f5135ba0a9b03eb04c4c0336271769
Serhii Kuzmychov <ku3cmich@gmail.com>
2024-02-01 08:32:20 +0200

    sync

--- xlog.h
+++ xlog.h
@@ -1,28 +1,40 @@
 #ifndef __xlog_h__
 #define __xlog_h__
 
+#include <xglob.h>
+#include <zephyr/logging/log.h>
+
 #define LOG_RESULT(action, ...) LOG_DBG("%s = %d", #action, action(__VA_ARGS__))
 
 #define ASSERT_NOT_NULL(action, value) __ASSERT(value != NULL, "%s got: NULL", #action);
 
 #define ASSERT_RESULT(action, ...)                      \
     {                                                   \
-        LOG_DBG("performing: %s", #action); \
+        LOG_DBG("performing: %s", #action);             \
         int res = action(__VA_ARGS__);                  \
         __ASSERT(res == 0, "%s got: %d", #action, res); \
     }
 
-#define SURE(action, ...)                      \
-    {                                                   \
-        int res = action(__VA_ARGS__);                  \
+#define SURE(action, ...)                                           \
+    {                                                               \
+        int res = action(__VA_ARGS__);                              \
         __ASSERT(res == 0, "unable to [%s] err: %d", #action, res); \
     }
 
-#define ENSURE(action, ...)                      \
-    {                                                   \
-        int res = action(__VA_ARGS__);                  \
+#define ENSURE(action, ...)                                         \
+    {                                                               \
+        int res = action(__VA_ARGS__);                              \
         __ASSERT(res != 0, "unable to [%s] err: %d", #action, res); \
     }
 
+#define fallback(n, s, ...)                                                  \
+    {                                                                        \
+        int err = s(__VA_ARGS__);                                            \
+        if (err)                                                             \
+        {                                                                    \
+            LOG_ERR("%s:%s err=%d", #s, STRINGY(ARGN(n, __VA_ARGS__)), err); \
+            return err;                                                      \
+        }                                                                    \
+    }
 
 #endif
